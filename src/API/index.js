f080b1abb8aeb0276ee02ed7a0b4e1ddd65c3ac4
Derek Miller <derekmiller@Dereks-MacBook-Pro.local>
2021-11-28 11:47:10 -0700

    completed all api calls

--- src/API/index.js
+++ src/API/index.js
@@ -106,6 +106,7 @@ export const fetchPublicRoutines = async () => {
     }
 }
 
+// this will fetch all the activities
 export const fetchAllActivities = async () => {
     try {
         const response = await fetch('http://fitnesstrac-kr.herokuapp.com/api/activities', {
@@ -123,5 +124,254 @@ export const fetchAllActivities = async () => {
     
     catch (error) {
         console.error(chalk.red('Trouble fetching all activities!!!'), error)
+    }
+}
+
+//this call will create a new activity with a valid token
+export const createActivity = async (token, name, description) => {
+    try {
+        const response = await fetch('http://fitnesstrac-kr.herokuapp.com/api/activities', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                name: name,
+                description: description
+            }),
+        })
+        const result = await response.json();
+        if (result.error) throw result.error;
+        console.log(result);
+        return result;
+    } 
+    
+    catch (error) {
+        console.error(chalk.red('Trouble creating activity!!!!'))
+        throw error;
+    }
+}
+
+//this will update an activity with a token, activityId, name, and description
+export const updateActivity = async (token, id, name, description) => {
+    try {
+        const response = await fetch(`http://fitnesstrac-kr.herokuapp.com/api/activities/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                name: name,
+                description: description
+            }),
+        })
+        const result = await response.json();
+        if (result.error) throw result.error;
+        console.log(result);
+        return result;
+    } 
+    
+    catch (error) {
+        console.error(chalk.red('Trouble updating activity!!!'));
+        throw error;
+    }
+}
+
+//this will return a list of public routines which feature that activity
+export const publicRoutinesByActivity = async (id) => {
+    try {
+        const response = await fetch(`http://fitnesstrac-kr.herokuapp.com/api/activities/${id}/routines`, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        const result = await response.json();
+        if (result.error) throw result.error;
+        console.log(result);
+        return result;
+    } 
+    
+    catch (error) {
+        console.error(chalk.red('Error getting public routines that feature that activity!!!'));
+        throw error;
+    }
+}
+
+
+//this will fetch all the current user's routines by their username and token
+export const getUserRoutines = async (username, token) => {
+    try {
+        const response = await fetch(`http://fitnesstrac-kr.herokuapp.com/api/users/${username}/routines`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+        })
+        const result = response.json();
+
+        if (result.error) throw result.error;
+        console.log(result);
+        return result;
+    } 
+    
+    catch (error) {
+        console.error(chalk.red('Trouble getting user routines!!!'));
+        throw error;
+    }
+}
+
+//this will attempt to create a new routine
+export const createRoutine = async (token, name, goal, isPublic) => {
+    try {
+        const response = await fetch('http://fitnesstrac-kr.herokuapp.com/api/routines', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                name: name,
+                goal: goal,
+                isPublic: isPublic
+            }),
+        })
+        const result = await response.json();
+        if (result.error) throw result.error;
+        console.log(result);
+        return result;
+    } 
+    
+    catch (error) {
+        console.error(chalk.red('ERROR creating new routine!!!'));
+        throw error;
+    }
+}
+
+//this will update a routine, notably change public/private, name, or goal
+export const updateRoutine = async (token, id, name, goal, isPublic) => {
+    try {
+        const response = await fetch(`http://fitnesstrac-kr.herokuapp.com/api/routines/${id}`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                name: name,
+                goal: goal,
+                isPublic: isPublic
+            }),
+        })
+        const result = await response.json();
+        if (result.error) throw result.error;
+        console.log(result);
+        return result;
+    } 
+    
+    catch (error) {
+        console.error(chalk.red('ERROR updating routine!!!'));
+        throw error;
+    }
+}
+
+//this will hard delete a routine. make sure to delete all the routineActivities whose routine is the one being deleted
+//will hard delete a routine whose id is equal to routineId
+export const deleteRoutine = async (token, id) => {
+    try {
+        const response = await fetch(`http://fitnesstrac-kr.herokuapp.com/api/routines/${id}`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+        })
+        const result = await response.json();
+        if (result.error) throw result.error;
+        console.log(result);
+        return result;
+    } 
+    
+    catch (error) {
+        console.error(chalk.red('ERROR deleting routine!!!'));
+        throw error;
+    }
+}
+
+//this attaches a single activity to a routine. prevents duplication on (routineId, activityId) pair
+export const attachActivityToRoutine = async (token, id, actId, count, duration) => {
+    try {
+        const response = await fetch(`http://fitnesstrac-kr.herokuapp.com/api/routines/${id}/activities`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                activityId: actId,
+                count: count,
+                duration: duration
+            }),
+        })
+        const result = await response.json();
+        if (result.error) throw result.error;
+        console.log(result);
+        return result;
+    } 
+    
+    catch (error) {
+        console.error(chalk.red('ERROR attaching activity to routine!!!'));
+        throw error;
+    }
+}
+
+//this updates the count or duration on routine activity
+export const updateOnRoutineActivity = async (token, id, count, duration) => {
+    try {
+        const response = await fetch(`http://fitnesstrac-kr.herokuapp.com/api/routine_activities/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": 'application/json',
+                'Authorizaion': 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                count: count,
+                duration: duration
+            }),
+        })
+        const result = await response.json();
+        if (result.error) throw result.error;
+        console.log(result);
+        return result;
+    } 
+    
+    catch (error) {
+        console.error(chalk.red('ERROR updating count or duration on routine activity!!!'));
+        throw error;
+    }
+}
+
+//this will remove an activity from a routine(hard deleting routine_activity)
+//dissociating an activity from a routine
+export const deleteActivityFromRoutine = async (token, id) => {
+    try {
+        const response = await fetch(`http://fitnesstrac-kr.herokuapp.com/api/routine_activities/${id}`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+        })
+        const result = response.json();
+        if (result.error) throw result.error;
+        console.log(result);
+        return result;
+    } 
+    
+    catch (error) {
+        console.error(chalk.red('ERROR deleting activity from routine!!!'));
+        throw error;
     }
 }
--- src/Components/myroutines.js
+++ src/Components/myroutines.js
@@ -1,11 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { userData } from '../API';
 
-const Myroutines = () => {
+const Myroutines = (props) => {
+
+//    const token = props.token;
+//    const history = props.history;
+//    const routines = props.routines;
+//    const setRoutines = props.setRoutines;
+//    const activites = props.activites;
+//    const setActivities = props.setActivities;
+
+//    const [name, setName] = useState("");
+//    const [goal, setGoal] = useState("");
+//    const [addActivity, setAddActivity] = ([]);
+//    const [userRoutines, setUserRoutines] = ([]);
+//    const [user, setUser] = ([]);
+
+//    useEffect(async () => {
+//     try {
+//         if (token) {
+//     const result = await userData(token);
+//     console.log("user data is", result);
+//     const routines = result.routines;
+//     const messages = result.;
+//     setUserPosts(posts);
+//     setUserMessages(messages);}
+//     }
+//     catch (err) {
+//         console.error("trouble", err)
+//     }
+    
+// }, []);
 
 
 
-}
+
+// }
 
 
 export default Myroutines;
